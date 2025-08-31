7ed7450412878591a9252d440dd420110df5b4ac
johnpaulalambra-design <johnpaul.alambra@unc.edu.ph>
2025-08-31 23:22:56 +0800

    Add files via upload
    basket

---
+++ urls.py
@@ -0,0 +1,4 @@
+from django.urls import path
+from . import views
+app_name='basket'
+urlpatterns=[path('',views.basket_detail,name='detail')]
---
+++ views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def basket_detail(request):
+    return render(request,'basket/detail.html')
