8de4843c3c9bc0872322866817a993ea8bcd3746
johnpaulalambra-design <johnpaul.alambra@unc.edu.ph>
2025-08-31 23:24:05 +0800

    Add files via upload
    campus store

--- urls.py
+++ urls.py
@@ -1,4 +1,13 @@
-from django.urls import path
-from . import views
-app_name='basket'
-urlpatterns=[path('',views.basket_detail,name='detail')]
+
+from django.contrib import admin
+from django.urls import path, include
+from main.views import HomeView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home'),
+    path('inventory/', include('inventory.urls')),
+    path('basket/', include('basket.urls')),
+    path('checkout/', include('checkout.urls')),
+    path('users/', include('users.urls')),
+]
